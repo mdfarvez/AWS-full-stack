fb8cf4da9ed749d7ff93790452ae030d87f9b789
MD FARVEZ <mdfarvez49@gmail.com>
2020-03-31 15:18:02 +0200

    First commit

---
+++ src/server.ts
@@ -0,0 +1,44 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { filterImageFromURL, deleteLocalFiles } from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // image processing
+  app.get("/filteredimage", async (req, res) => {
+    const imageURL = req.query.image_url;
+
+    if (!imageURL) {
+      return res.status(400).send("Image URL required!");
+    }
+
+    const imagePath = await filterImageFromURL(imageURL);
+
+    res.status(200).sendFile(imagePath, () => {
+      deleteLocalFiles([imagePath]);
+    });
+
+  });
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get("/", async (req, res) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  });
+
+
+  // Start the Server
+  app.listen(port, () => {
+    console.log(`server running http://localhost:${port}`);
+    console.log(`press CTRL+C to stop server`);
+  });
+})();
